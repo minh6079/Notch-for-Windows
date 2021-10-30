5251caaefe8089227c0ca002c0ea4bfb51bed484
Maanuj <maanujvora@gmail.com>
2021-10-29 23:42:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import tkinter as tk
 
+root = tk.Tk()
+
 def unmap(event):
     if event.widget is root:
         root.deiconify()
         
-root = tk.Tk()
 
-ws = root.winfo_screenwidth()
-hs = root.winfo_screenheight()
+def screen_stuff():
+    
+    ws = root.winfo_screenwidth()
+    hs = root.winfo_screenheight()
 
-notchx = int(ws/9)
-notchy = int(hs/30)
+    notchx = int(ws/9)
+    notchy = int(hs/30)
 
-leftx = (ws/2) - (notchx/2)
-rightx = (ws/2) + (notchx/2)
+    leftx = (ws/2) - (notchx/2)
+    rightx = (ws/2) + (notchx/2)
+
+    root.geometry('%dx%d+%d+%d' % (notchx, notchy, leftx, 0))
+
+screen_stuff()
+
+root.configure(background='black')
 
 root.protocol('WM_DELETE_WINDOW', lambda: None)
-root.geometry('%dx%d+%d+%d' % (notchx, notchy, leftx, 0))
 root.overrideredirect(True)
-root.configure(background='black')
 root.attributes('-topmost',True)
 root.bind('<Unmap>', unmap)
 
